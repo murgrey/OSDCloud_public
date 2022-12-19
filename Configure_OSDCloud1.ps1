492986166b8e5681cc8dc8da3fbd9dfe9652635d
murgrey <100181177+murgrey@users.noreply.github.com>
2022-12-19 14:39:31 -0700

    Create Configure_OSDCloud1.ps1

---
+++ Configure_OSDCloud1.ps1
@@ -0,0 +1,41 @@
+Write-Host  -ForegroundColor Cyan "Starting Custom OSDCloud ..."
+Start-Sleep -Seconds 5
+
+#Change Display Resolution for Virtual Machine
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
+}
+
+#Make sure I have the latest OSD Content
+Write-Host  -ForegroundColor Cyan "Updating the awesome OSD PowerShell Module"
+Install-Module OSD -Force
+
+Write-Host  -ForegroundColor Cyan "Importing the sweet OSD PowerShell Module"
+Import-Module OSD -Force
+
+ $Global:OSDCloud = @{
+ 	ApplyManufacturerDrivers = $True
+	ApplyCatalogDrivers = $True
+	ApplyCatalogFirmware = $True
+    OSBuild = '22H2' 
+    OSName = 'Windows 10 22H2 x64'
+    OSEdition = 'Pro '
+    OSEditionId = 8
+    OSVersion = 'Windows 10'
+    ScreenshotCapture = $True 
+    ZTI = $True 
+ }
+
+  $Global:MyOSDCloud = @{
+ 	ApplyManufacturerDrivers = $True
+	ApplyCatalogDrivers = $True
+	ApplyCatalogFirmware = $True
+    OSBuild = '22H2' 
+    OSName = 'Windows 10 22H2 x64'
+    OSEdition = 'Pro '
+    OSEditionId = 8
+    OSVersion = 'Windows 10'
+    ScreenshotCapture = $True 
+    ZTI = $True 
+ }
