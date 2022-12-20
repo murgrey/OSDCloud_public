15129ace16aa41fbbeff931f3b657ccb73ae2524
murgrey <100181177+murgrey@users.noreply.github.com>
2022-12-20 06:52:51 -0700

    Update Configure_OSDCloud1.ps1

--- Configure_OSDCloud1.ps1
+++ Configure_OSDCloud1.ps1
@@ -7,17 +7,6 @@ if ((Get-MyComputerModel) -match 'Virtual') {
     Set-DisRes 1600
 }
 
-Start-OSDCloud -ZTI -Restart -Screenshot -Firmware -OSLanguage en-us -OSVersion 'Windows 10' -OSBuild '22H2' -OSEdition Pro -OSLicense Volume
-
-Write-Host -ForegroundColor Green "Define Computername:"
-$TargetComputername = Get-WmiObject Win32_Bios | Select-Object -ExpandProperty SerialNumber
-
-# Update Windows
-    UpdateWindows
-   
-# Update Drivers
-    UpdateDrivers
-
 Set-ExecutionPolicy -ExecutionPolicy ByPass -Force
 
 Write-Verbose -Verbose 'Install PowerShell Module AutopilotOOBE'
@@ -26,14 +15,31 @@ Install-Module AutopilotOOBE -Force
 Write-Verbose -Verbose 'Install PowerShell Script Get-WindowsAutoPilotInfo'
 Install-Script -Name Get-WindowsAutoPilotInfo -Force
 
+<#
 Write-Verbose 'Run AutopilotOOBE in the new PowerShell window' -Verbose
 Start-Sleep -Seconds 5
 Start-Process PowerShell.exe
+#>
+# Begin Autopilot Enrollment
+    Get-WindowsAutoPilotInfo -Online 
+    Start-Sleep -Seconds 5
 
-Get-WindowsAutoPilotInfo -Online 
+# Install Windows 10
+    Start-OSDCloud -ZTI -Screenshot -Firmware -OSLanguage en-us -OSVersion 'Windows 10' -OSBuild '22H2' -OSEdition Pro -OSLicense Volume # -Restart
+
+Write-Host -ForegroundColor Green "Define Computername:"
+$TargetComputername = Get-WmiObject Win32_Bios | Select-Object -ExpandProperty SerialNumber
+
+# Update Windows
+    UpdateWindows
+    Start-Sleep -Seconds 5
+
+# Update Drivers
+    UpdateDrivers
+    Start-Sleep -Seconds 5
 
 #Restart from WinPE
-Read-Host -Prompt "Please remove boot media now and press any key to continue..."
-Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
-Start-Sleep -Seconds 20
-wpeutil reboot
+    Read-Host -Prompt "Please remove boot media now and press any key to continue..."
+    Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
+    Start-Sleep -Seconds 20
+    wpeutil reboot
