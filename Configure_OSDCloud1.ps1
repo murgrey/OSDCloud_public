28eb6a8da77ddfece274e942e1dc69e5a9c40ad5
murgrey <100181177+murgrey@users.noreply.github.com>
2022-12-19 20:04:40 -0700

    Update Configure_OSDCloud1.ps1

--- Configure_OSDCloud1.ps1
+++ Configure_OSDCloud1.ps1
@@ -7,32 +7,18 @@ if ((Get-MyComputerModel) -match 'Virtual') {
     Set-DisRes 1600
 }
 
-Start-OSDCloud -ZTI -Restart -Screenshot -Firmware -OSLanguage en-us -OSVersion 'Windows 10' -OSBuild '22H2' -OSEdition Pro -OSLicense Volume 
+Start-OSDCloud -ZTI -Restart -Screenshot -Firmware -OSLanguage en-us -OSVersion 'Windows 10' -OSBuild '22H2' -OSEdition Pro -OSLicense Volume
 
-Restart from WinPE
-Read-Host -Prompt "Please remove boot media now and press any key to continue..."
-Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
-Start-Sleep -Seconds 20
-wpeutil reboot
+Write-Host -ForegroundColor Green "Define Computername:"
+$TargetComputername = Get-WmiObject Win32_Bios | Select-Object -ExpandProperty SerialNumber
+
+# Update Windows
+    UpdateWindows
+   
+# Update Drivers
+    UpdateDrivers
+
 Set-ExecutionPolicy -ExecutionPolicy ByPass -Force
-
-if (-not (Get-PackageProvider | Where-Object {$_.Name -eq 'NuGet'}))
-{
-    Write-Verbose -Verbose 'Install PackageProvider NuGet'
-    Install-PackageProvider -Name NuGet -Force
-}
-
-if ((Get-Module PowerShellGet).version -lt [System.Version]'2.2.5.0')
-{
-    Write-Verbose -Verbose 'Install PowerShell Module PowerShellGet'
-    Install-Module PowerShellGet -Force
-}
-
-if ((Get-Module PackageManagement).version -lt [System.Version]'1.4.7.0')
-{
-    Write-Verbose -Verbose 'Install PowerShell Module PackageManagement'
-    Install-Module PackageManagement -Force
-}
 
 Write-Verbose -Verbose 'Install PowerShell Module AutopilotOOBE'
 Install-Module AutopilotOOBE -Force
@@ -45,3 +31,9 @@ Start-Sleep -Seconds 5
 Start-Process PowerShell.exe
 
 Get-WindowsAutoPilotInfo -Online 
+
+#Restart from WinPE
+Read-Host -Prompt "Please remove boot media now and press any key to continue..."
+Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
